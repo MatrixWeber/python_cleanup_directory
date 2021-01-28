48a9f3c6363eddd9b537ba0b8c5575fffb0ac1ac
Alexander Weber <alexande.weber@gmail.com>
2021-01-28 21:38:08 +0100

    new directories to remove added and cleanup a bit

--- remove_debug_stuff_from_directory.py
+++ remove_debug_stuff_from_directory.py
@@ -2,24 +2,35 @@
 
 import os
 import shutil
-from time import sleep
 import fnmatch
 from check_if_dir_exists import dir_exists
 
+pathOfDirToLookFor = r'C:\Users\SW\Desktop\MicroConsult\Clean Code\Uebungen'
 
-pathOfDirToLookFor = '/home/z002wydr/Documents/Schulungen/Microconsult/OL-OOPFCPP_AW/Exercises'
 
-while True:
-    if dir_exists(pathOfDirToLookFor):
-        dirNameToFilter = ['cmake-build-debug', 'cmake-build-release', 'build']
-        matches = []
-        for root, dirnames, filenames in os.walk(pathOfDirToLookFor):
-            for name in dirNameToFilter:
-                for dirname in fnmatch.filter(dirnames, name):
-                    matches.append(os.path.join(root, dirname))
-        for match in matches:
-            if os.path.isdir(match):
+def remove_directories_from_path(path_of_dir_to_look_for, dir_name_to_filter):
+    matches = find_directories(path_of_dir_to_look_for, dir_name_to_filter)
+    remove_directories(matches)
+
+
+def remove_directories(matches):
+    for match in matches:
+        if os.path.isdir(match):
+            try:
                 shutil.rmtree(match)
-    else:
-        pathOfDirToLookFor = input('Path to the directory to remove debug stuff: ')
-    sleep(10)
+            except:
+                continue
+
+
+def find_directories(path_of_dir_to_look_for, dir_name_to_filter):
+    matches = []
+    for root, directoryNames, filenames in os.walk(path_of_dir_to_look_for):
+        for name in dir_name_to_filter:
+            for dirname in fnmatch.filter(directoryNames, name):
+                matches.append(os.path.join(root, dirname))
+    return matches
+
+
+if dir_exists(pathOfDirToLookFor):
+    dirNameToFilter = ['Release', 'Debug', '.vs', '.vscode', 'out', 'cmake-build-debug-cygwin', 'cmake-build-debug-visual-studio', 'cmake-build-debug', 'cmake-build-release', 'build']
+    remove_directories_from_path(pathOfDirToLookFor, dirNameToFilter)
