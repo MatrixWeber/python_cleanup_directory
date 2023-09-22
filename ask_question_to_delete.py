d16b58efc1c55a91a9a22e57ed169c408b903987
Alexander <alexander.weber@siemens-energy.com>
2023-09-22 08:09:58 +0200

    add a print if verbose mode

--- ask_question_to_delete.py
+++ ask_question_to_delete.py
@@ -13,6 +13,7 @@ def askQuestionAndPerform(destDirection, fileName, verbose, sayYesToString = '/]
     else:
         if verbose:
             notify('Cleanup Script', 'Sure you wanna replace that file: ' + destFile + ' ???\n' + chooseString + '\n')
+            print('Cleanup Script', 'Sure you wanna replace that file: ' + destFile + ' ???\n' + chooseString + '\n')
         deleteOptions = input('Sure you wanna replace that file: ' + destFile + ' ???\n' + chooseString + '\n')
     if "yes" in deleteOptions or "y" in deleteOptions:
         if os.path.isdir(destFile):
@@ -21,6 +22,7 @@ def askQuestionAndPerform(destDirection, fileName, verbose, sayYesToString = '/]
             os.remove(destFile)
         if verbose:
             notify('Cleanup Script', destFile + ': was moved to trash\n')
+            print('Cleanup Script', destFile + ': was moved to trash\n')
     elif "o" in deleteOptions or "open" in deleteOptions:
         if platform.system() == 'Darwin':       # macOS
             subprocess.call(('open', destFile))
@@ -28,19 +30,22 @@ def askQuestionAndPerform(destDirection, fileName, verbose, sayYesToString = '/]
             os.startfile(destFile)
         else:                                   # linux variants
             subprocess.call(('xdg-open', destFile))
-        askQuestionAndPerform(destDirection, fileName)
+        askQuestionAndPerform(destDirection, fileName, verbose)
     elif "r" in deleteOptions or "rename" in deleteOptions:
         if verbose:
             notify('Cleanup Script', 'Enter new file name!\n')
+            print('Cleanup Script', 'Enter new file name!\n')
         newFileName = input("Enter new file name!\n")
         os.rename(destFile, destDirection + '/' + newFileName)
     elif "n" in deleteOptions or "no" in deleteOptions:
         if verbose:
             notify('Cleanup Script', 'Let this file untouched!\n')
+            print('Cleanup Script', 'Let this file untouched!\n')
         os.rename(destFile, destFile + "_copy")
     elif "a" in deleteOptions:
         if verbose:
             notify('Cleanup Script', 'Remove all files from dir!\n')
+            print('Cleanup Script', 'Remove all files from dir!\n')
         print('Directories and Files to remove: ')
         for file in os.listdir(pathToDefaultDirectory):
             fileNameStr = str(file)
